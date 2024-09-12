8ffa039c076bc8aa7f8045d79c74d483efbe28a8
Leo-guerrero <sja.leo.guerrero@gmail.com>
2024-09-12 17:54:18 -0500

    Add files via upload

---
+++ Grading Distribution/Grading Distribution/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Grading Distribution
+//
+//  Created by Turing on 9/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+
